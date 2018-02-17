6535a6c27ae730f52e57c0ae93950a34cb4f6bf2
raggaBatz <erik6594@gmail.com>
2018-02-16 20:58:13 -0600

    Adicion de logica de movimientos a clase principal

--- Tiendita/src/tiendita/Tiendita.java
+++ Tiendita/src/tiendita/Tiendita.java
@@ -5,13 +5,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
 
-/**
- * @author RAGGA-PC
- */
 public class Tiendita {
     ArrayList<Cliente> listaClientes = new ArrayList<>();
     ArrayList<Tienda> listaTiendas = new ArrayList<>();
+    ArrayList<Movimiento> listaMovimientos = new ArrayList<>();
     
     public static void main(String[] args) {
        Tiendita t = new Tiendita();
@@ -20,6 +20,54 @@ public class Tiendita {
     
     public void principal(){
         cargar();
+        Scanner op = new Scanner( System.in );
+        int val = 5;
+        do{
+            System.out.println("---MENU-PRINCIPAL---");
+            System.out.println("--1.Mantenimientos--");
+            System.out.println("--2.Movimientos-----");
+            System.out.println("--0.Salir-----");
+            val = op.nextInt();
+            if(val==1){
+                //gestionarMantenimientos();
+            }
+            if(val==2){
+                System.out.println("***************************");
+                System.out.println("-----MOVIMIENTOS-----");
+                gestionarMovimientos();
+            }
+        }while(val!=0);
+    }
+    
+    public void gestionarMovimientos(){
+        Scanner op = new Scanner( System.in );
+        int idMovimiento = obtenerNumeroMovimiento();
+        System.out.println("***************************");
+        System.out.println("--Monto--");
+        System.out.println("--2.Movimientos-----");
+        System.out.println("--0.Salir-----");
+        int val = op.nextInt();
+        if(val==1){
+            //gestionarMantenimientos();
+        }
+        if(val==2){
+            gestionarMovimientos();
+        }
+    }
+    
+    public int obtenerNumeroMovimiento(){
+        int id = 0;
+        if(listaMovimientos.isEmpty()){
+            Calendar cal = Calendar.getInstance();
+            int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+            id = dayOfMonth + 0001;
+        }else{
+            for (int i = 0; i < listaMovimientos.size(); i++) {
+                id = listaMovimientos.get(i).getId();
+            }
+        }
+        id++;
+        return id;
     }
     
     public void cargar(){
